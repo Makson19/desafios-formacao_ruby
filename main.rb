67b685cb03d6c6441b84adc96d02c6cd8d5ca26f
Makson19 <makson.douglas@gmail.com>
2026-04-13 17:50:54 -0300

    finalizando desafio01 de curso de ruby

---
+++ main.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome:  "
+nome = gets.chomp
+
+print "Digite seu sobrenome:  "
+sobrenome = gets.chomp
+
+print "Digite sua idade:  "
+idade = gets.chomp.to_i
+
+puts "Olá! Meu nome é #{nome} #{sobrenome} e tenho #{idade} anos de idade."
